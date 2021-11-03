ec2465498fc0742712bb377a6e6f4655a4e60113
Drancer55 <antonio281255@gmail.com>
2021-11-02 21:20:32 -0600

    Agregando pseudocódigo necesario en cada area

--- src/dataScript.js
+++ src/dataScript.js
@@ -1,10 +1,11 @@
+//Gracias al fetch se puede establecer comunicación con los datos del JSON
 fetch("../assets/dataJSON/Data.json")
 .then((response) => response.json())
 .then((data) => iterarCards(data))
 .catch((error) => console.log(error))
-
+//"cards" es el contenedor del "Gameplay" en el que se iteran las tarjetas
 let tarjetas = document.getElementById("cards")
-
+//Aquí se "pintan" los datos textuales necesarios para hacer las tarjetas
 let iterarCards = (data) => {
     console.log(data)
     for (const tarjeta of data.Cards_TLOZ) {
@@ -15,7 +16,7 @@ let iterarCards = (data) => {
         <div class="card-cover" id="${tarjeta.Titulo}">${tarjeta.Titulo}</div>
     </div>
     </div>`
-
+//Aquí se "pinta" la portada de cada tarjeta
 let portadaCard = tarjeta.Imagen;
 let portadaTarjeta = document.getElementById(`${tarjeta.Titulo}`)
 portadaTarjeta.style.backgroundImage = "url(" + portadaCard + ")"
